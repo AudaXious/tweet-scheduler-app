3c53368359cf0ec906a4745ceb8c99bac1c3e206
TinkerPal <tinkerpi2023@gmail.com>
2023-12-09 16:47:30 -0500

    Added account creation

--- src/components/LinkedTwitter.jsx
+++ src/components/LinkedTwitter.jsx
@@ -1,7 +1,16 @@
 export default function LinkedTwitter({ authUser }) {
   return (
     <div style={{ marginTop: "5px", marginBottom: "5px" }}>
-      <h4>Linked Twitter: {authUser.name}</h4>
+      <h4>
+        Linked Twitter:{" "}
+        <a
+          href={`https://twitter.com/${authUser.name}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {authUser.screenName}
+        </a>
+      </h4>
     </div>
   );
 }
